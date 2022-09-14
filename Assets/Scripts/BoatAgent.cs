dac5c6c732582bf34a5f67736243ac1e652a5d71
Riddhi More <more.riddhi11@gmail.com>
2022-09-14 11:22:34 +0530

    initialised repository with two training grounds and agents

---
+++ Assets/Scripts/BoatAgent.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class BoatAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    [SerializeField] private GameObject boat;
+    private int epNo = 0;
+
+
+    public override void OnEpisodeBegin()
+    {
+        epNo += 1;
+        Debug.Log(epNo);
+        pointB.transform.localPosition = new Vector3(Random.Range(-1.6f, 1.3f), 0.41f, Random.Range(2.5f, 4f));
+        GameObject.Find("BoatParent").transform.localPosition = new Vector3(-3f, 0.7f, -3f);
+       
+        //transform.position = pointA.transform.position + new Vector3(0.5f, 0, 0.5f);
+
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+        sensor.AddObservation(pointA.transform.localPosition);
+        sensor.AddObservation(pointB.transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float MoveX = actions.ContinuousActions[0];
+        float MoveZ = actions.ContinuousActions[1];
+        float moveSpeed = 5f;
+
+        GameObject.Find("BoatParent").transform.localPosition += new Vector3(MoveX, 0, MoveZ) * Time.deltaTime * moveSpeed;
+        
+        Vector3 movementDir = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        if (movementDir != Vector3.zero)
+        {
+            GameObject.Find("BoatParent").transform.forward = movementDir;
+        }
+    }
+    //for testing purpose only
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1.0f);
+            boat.GetComponent<Renderer>().material.color = Color.red;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Whale>(out Whale whale))
+        {
+            SetReward(-1.0f);
+            boat.GetComponent<Renderer>().material.color = Color.magenta;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+2.0f);
+            boat.GetComponent<Renderer>().material.color = Color.green;
+            EndEpisode();
+
+        }
+
+ 
+
+    }
+}

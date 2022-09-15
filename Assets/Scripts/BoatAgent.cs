89d4ae33d25753e6d61bb845b377aa8eaf149053
Riddhi More <more.riddhi11@gmail.com>
2022-09-15 14:19:09 +0530

    changed enviornment and rewards

--- Assets/Scripts/BoatAgent.cs
+++ Assets/Scripts/BoatAgent.cs
@@ -15,7 +15,8 @@ public class BoatAgent : Agent
     //[SerializeField] private GameObject episode;
     private int epNo = 0;
     private float accuracy = 0;
-
+    [SerializeField]float moveSpeed = 5f;
+    private float prevDis;
     public override void OnEpisodeBegin()
     {
         epNo += 1;
@@ -23,7 +24,7 @@ public class BoatAgent : Agent
         //episode.GetComponent<Text>().text = "Episode: " + epNo;
         pointB.transform.localPosition = new Vector3(Random.Range(-1.6f, 1.3f), 0.41f, Random.Range(2.5f, 4f));
         transform.parent.transform.localPosition = new Vector3(-3f, 0.7f, -3f);
-       
+        
         //transform.position = pointA.transform.position + new Vector3(0.5f, 0, 0.5f);
 
     }
@@ -31,7 +32,7 @@ public class BoatAgent : Agent
     {
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(targetTransform.localPosition);
-        sensor.AddObservation(pointA.transform.localPosition);
+        //sensor.AddObservation(pointA.transform.localPosition);
         sensor.AddObservation(pointB.transform.localPosition);
     }
 
@@ -39,14 +40,14 @@ public class BoatAgent : Agent
     {
         float MoveX = actions.ContinuousActions[0];
         float MoveZ = actions.ContinuousActions[1];
-        float moveSpeed = 5f;
+       
 
         transform.parent.transform.localPosition += new Vector3(MoveX, 0, MoveZ) * Time.deltaTime * moveSpeed;
         
         Vector3 movementDir = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
         if (movementDir != Vector3.zero)
         {
-            transform.parent.transform.forward = movementDir;
+            //transform.parent.transform.forward = movementDir;
         }
     }
     //for testing purpose only
@@ -56,23 +57,44 @@ public class BoatAgent : Agent
         continuousActions[0] = Input.GetAxisRaw("Horizontal");
         continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
+    private void Update()
+    {
+        float prevDis = 999f;
+        float currDis = Vector3.Distance(boat.transform.localPosition, pointB.transform.localPosition);
+        if (currDis < prevDis)
+        {
+            SetReward(+0.1f);
+        }
+        else
+        {
+            SetReward(-0.1f);
+        }
+        prevDis = currDis;
+    }
     private void OnTriggerEnter(Collider other)
     {
+       
         if(other.TryGetComponent<Wall>(out Wall wall))
         {
-            SetReward(-2.0f);
+            SetReward(-1.0f);
             boat.GetComponent<Renderer>().material.color = Color.red;
-            EndEpisode();
+            //EndEpisode();
         }
         if (other.TryGetComponent<Whale>(out Whale whale))
         {
-            SetReward(-3.0f);
+            SetReward(-1.0f);
             boat.GetComponent<Renderer>().material.color = Color.magenta;
-            EndEpisode();
+            //EndEpisode();
+        }
+        if (other.TryGetComponent<Checkpoint>(out Checkpoint cp))
+        {
+            SetReward(+1.0f);
+            boat.GetComponent<Renderer>().material.color = Color.yellow;
+            
         }
         if (other.TryGetComponent<Goal>(out Goal goal))
         {
-            SetReward(+2.0f);
+            SetReward(+1.0f);
             boat.GetComponent<Renderer>().material.color = Color.green;
             EndEpisode();
             accuracy += 1;
